18643f2b9c199c413b28a4c2221c0dfa754043ed
Andy---- <andypollard84@gmail.com>
2016-02-08 13:38:45 +0000

    week3

---
+++ week1/simple.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Performance stats for $USER"
+
+cnt=`ps -ef | wc -l`
+
+echo "There are $cnt processes running"
+echo "$1 is the limit"
+
+if [ "$cnt" -gt "$1" ]
+ then
+  echo "limit exceeded"
+fi
