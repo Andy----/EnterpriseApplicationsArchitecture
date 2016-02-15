41f6d99507ad3b9e38030ce2bc408d0f0aa08851
Andrew Pollard <Andy----@users.noreply.github.com>
2016-02-15 13:51:28 +0000

    Create week3script1.sh
    week3

---
+++ week3/week3script1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ "$USER" != "eaauser" ]; then
+        echo 'Access Denied'
+        exit 1
+fi
+
+#: Description: print Hello and the first command-line argument
+printf "Hello $1 you were born in $2 and your favourite colour is $3\n"
